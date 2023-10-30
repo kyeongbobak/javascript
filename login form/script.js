ce3236ce95c928371d14f311db2364b51dd07771
kyeongboo-coder <kyeong0914@gmail.com>
2023-10-30 20:39:39 +0900

    javscript 추가

---
+++ login form/script.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector(".login-form");
+const loginFormId = document.querySelector(".login-form-id");
+const loginFormPassword = document.querySelector(".login-form-password");
+const loginFormButton = document.querySelector(".login-form-button");
+
+const handleOnClick() {
+
+}
+
+loginFormButton.addEventListener("click",handleOnClick)
