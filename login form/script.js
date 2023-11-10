7a9097b5757e4c8076289e8ac308633ea37782d0
kyeongboo-coder <kyeong0914@gmail.com>
2023-11-10 14:15:45 +0900

    feat : 유효성 검사 기능, 비밀번호 숨기기 기능 구현

--- login form/script.js
+++ login form/script.js
@@ -1,5 +1,6 @@
 const loginFormPassword = document.querySelector(".login-form-password");
 const validPass = document.querySelector(".valid-pass");
+const loginFormPoint = document.querySelector(".login-form-point");
 
 function formValid() {
   if (loginFormPassword.value.length >= 1) {
@@ -14,9 +15,9 @@ loginFormPassword.addEventListener("input", formValid);
 function show() {
   if (loginFormPassword.type !== "password") {
     loginFormPassword.type = "password";
-    document.querySelector(".login-form-point").src = "https://cdn2.iconfinder.com/data/icons/basic-ui-interface-v-2/32/hide-512.png";
+    loginFormPoint.src = "https://cdn2.iconfinder.com/data/icons/basic-ui-interface-v-2/32/hide-512.png";
   } else {
     loginFormPassword.type = "type";
-    document.querySelector(".login-form-point").src = "https://static.thenounproject.com/png/777494-200.png";
+    loginFormPoint.src = "https://static.thenounproject.com/png/777494-200.png";
   }
 }
